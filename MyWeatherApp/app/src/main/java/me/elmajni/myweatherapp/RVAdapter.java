8bb5ef5738513847be386ab3e7720e7cbd11872f
khaoula elmajni <elmajnikhaoula99@gmail.com>
2022-05-14 01:33:58 +0100

    make some changes

--- MyWeatherApp/app/src/main/java/me/elmajni/myweatherapp/RVAdapter.java
+++ MyWeatherApp/app/src/main/java/me/elmajni/myweatherapp/RVAdapter.java
@@ -1,4 +1,44 @@
 package me.elmajni.myweatherapp;
 
-public class RVAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<Model> arrayList;
+
+    public RVAdapter(Context context, ArrayList<Model> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public RVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RVAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
