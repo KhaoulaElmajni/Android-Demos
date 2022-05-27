0cec56dcaf2ddadcf7581eab047c3896e0d41bb0
khaoula elmajni <elmajnikhaoula99@gmail.com>
2022-05-27 13:31:57 +0100

    flutter 4

---
+++ listview2/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter App Learning',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> images = [
+    "assets/images/scenary.jpg",
+    "assets/images/scenary_red.jpg",
+    "assets/images/waterfall.jpg",
+    "assets/images/tree.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter ListView"),
+        ),
+        body: ListView.builder(
+          itemBuilder: (BuildContext, index){
+            return Card(
+              child: ListTile(
+                leading: CircleAvatar(backgroundImage: AssetImage(images[index]),),
+                title: Text("This is title"),
+                subtitle: Text("This is subtitle"),
+              ),
+            );
+          },
+          itemCount: images.length,
+          shrinkWrap: true,
+          padding: EdgeInsets.all(5),
+          scrollDirection: Axis.vertical,
+        )
+    );
+  }
+}
